6508791bba2b8e009f4ba23bedb78c9d4e805400
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-06 22:50:07 +0530

    addinfg const

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
+const name = 'Yash Kaskar'
   return (
     <div className="App">
       <div className="container">
         <h1>Hello React</h1>
         <p>Building the Articles</p>
+        <h2>{ name }</h2>
       </div>
     </div>
   );

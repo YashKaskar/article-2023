39f7bd8051e719cc2b820d40a569b604cc983f67
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-06 15:22:52 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Hello React</h1>
+        <h1>Hello React</h1>
+        <p>Building the Articles</p>
       </div>
     </div>
   );

43ac233b3644ede28d2997cdddc41357056173d9
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-07 10:44:38 +0530

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
 const name = 'Yash Kaskar'
   return (
     <div className="App">
+
+      <Navbar />
+
+
+
+
       <div className="container">
         <h1>Hello React</h1>
         <p>Building the Articles</p>

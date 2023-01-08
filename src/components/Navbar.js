5f21f350bec0c2a7ad06717306f48923531cfe08
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-08 10:15:07 +0530

    adding class to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 const Navbar = () => {
     return (    
         <nav className="navbar">   
-            <h1>Articles 2023</h1>
-            <a href="/">Home</a>
-          <a href="/create">New Article</a>  
+        <h1>Articles 2023</h1>
+        <div className="anchor">
+        <a href="/" id="anchor">Home</a>
+          <a href="/create" id="anchor">New Article</a>
+        </div>  
         </nav>
      );
 }
  
 export default Navbar;
+

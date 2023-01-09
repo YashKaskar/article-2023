e82ad1789f4ae31156f64eeea73e4a8fd6d82beb
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-09 13:07:34 +0530

    adding useState name and age

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,19 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('Kakashi')
+    const [name, setName] = useState('Kakashi');
+    const [age, setAge] = useState(30);
     
     const handleClick = () => {
         setName('Naruto');
+        setAge(25);
+
     }
 
     return (    
         <div className="home">
             <h1>HomePage</h1>
-            <p>{ name }</p>
+            <p>{name} is { age } year old</p>
             <button onClick={handleClick}>Click Me !!!!!</button>
         </div>
      );

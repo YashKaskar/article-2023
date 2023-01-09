6ff64308c4cefbcee357c23e95cc9698a62de894
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-09 12:41:41 +0530

    adding onClick at button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,13 @@ const Home = () => {
 
 
     const handleClick = () => {
-            
+
     }
 
     return (    
         <div className="home">
             <h1>HomePage</h1>
-            <button>Click Me !!!!!</button>
+            <button onClick={handleClick}>Click Me !!!!!</button>
         </div>
      );
 }

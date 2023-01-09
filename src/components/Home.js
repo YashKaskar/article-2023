794f4425a7f7af0e7901ca9deccc7908370e9037
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-09 12:59:40 +0530

    use State

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,16 @@ import { useState } from "react";
 
 const Home = () => {
 
-
+    const [name, setName] = useState('Kakashi')
+    
     const handleClick = () => {
-
+        setName('Naruto');
     }
 
     return (    
         <div className="home">
             <h1>HomePage</h1>
+            <p>{ name }</p>
             <button onClick={handleClick}>Click Me !!!!!</button>
         </div>
      );

e199c01473b109402b67cd99ce74988e87d7a3f8
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-11 11:19:05 +0530

    adding blog-details

--- src/BlogsList/BlogList.js
+++ src/BlogsList/BlogList.js
@@ -1,1 +1,14 @@
-sfc
+const BlogList = () => {
+    return (    
+        <div className="blog-list">
+        {blogs.map((blog) => (  
+                <div className="blog-details" key={blog.id}>
+                    <h2> {blog.title}</h2>
+                    <p>Written by the author : {blog.author}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;

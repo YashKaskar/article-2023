05c9cae010cef949de4491667c0fbc17f3ba383d
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-16 10:57:43 +0530

    changes

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (    
+        <nav>   
+            <h1>Articles 2023</h1>
+            <a href="/">Home</a>
+          <a href="/create">New Article</a>  
+        </nav>
+     );
+}
+ 
+export default Navbar;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,25 @@ const Home = () => {
         { title: 'My new article 2023', body: 'Lorem, ipsum ...', author: 'obito', id: 3 }
     ])
 
+    const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {  
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+
+    useEffect(() => {
+        console.log('useffect');
+        console.log(name);
+    }, [name]);
+
+
     return (    
         <div className="home">
-            < BlogList blogs={blogs} title='All Articles' handleDelete= {handleDelete} />
+            < BlogList blogs={blogs} title='All Articles' handleDelete={handleDelete} />
+            <button onClick={() => setName('lugia')}>change name</button>
+            <p>{ name }</p>
             {/* <BlogList blogs={blogs.filter((blogs) => blogs.author === 'obito') } title = 'Obito Blogs' /> Reusing Components */}
         </div>
      );

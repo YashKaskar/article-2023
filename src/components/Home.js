d6ac9c9dda698c5a2bc46fc698f04ea678bbf647
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-16 13:55:07 +0530

    conditional loading message

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,22 @@ import BlogList from './../BlogsList/BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true);
 
 
     useEffect(() => {
         fetch('http://localhost:8080/blogs')
         .then(res => { return res.json() })  //to get res from db.json file
-        .then(data => {setBlogs(data)})   //having data from db.json file
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false)
+            })   //having data from db.json file
     }, []);
 
 
     return (    
         <div className="home">
+            {isPending && <div> Loading..............</div>}
             { blogs && < BlogList blogs={blogs} title='All Articles'  />}
             {/* <BlogList blogs={blogs.filter((blogs) => blogs.author === 'obito') } title = 'Obito Blogs' /> Reusing Components */}
         </div>

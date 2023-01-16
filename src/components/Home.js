ba27c3e2f1c6bbade049f2ce8780547b3f6c20e7
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-16 13:47:34 +0530

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,6 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null)
 
 
-    const handleDelete = (id) => {  
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-
     useEffect(() => {
         fetch('http://localhost:8080/blogs')
         .then(res => { return res.json() })  //to get res from db.json file
@@ -22,7 +16,7 @@ const Home = () => {
 
     return (    
         <div className="home">
-            { blogs && < BlogList blogs={blogs} title='All Articles' handleDelete={handleDelete} />}
+            { blogs && < BlogList blogs={blogs} title='All Articles'  />}
             {/* <BlogList blogs={blogs.filter((blogs) => blogs.author === 'obito') } title = 'Obito Blogs' /> Reusing Components */}
         </div>
      );

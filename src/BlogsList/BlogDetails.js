8895df31fbe9daf53445b3eb1a2c843f837d19a4
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-19 13:30:48 +0530

    changes

--- src/BlogsList/BlogDetails.js
+++ src/BlogsList/BlogDetails.js
@@ -1,16 +1,17 @@
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
+
+
 
 const BlogDetails = () => {
-
     const { id } = useParams();
+   
+  
 
-
-
-    return (    
-    <div className="blogdetails">
-            <h2>Blog Details - { id }</h2>
-    </div>    
-    );
+  return (
+      <div>  
+          <h2>Blog Details - { id }</h2>
+     </div>
+  );
 }
  
 export default BlogDetails;

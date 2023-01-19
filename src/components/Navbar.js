9d6289a0204b74fc16f2142740ffbe506767e687
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-19 11:00:41 +0530

    router link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (    
         <nav className="navbar">   
         <h1>Articles 2023</h1>
         <div className="anchor">
-        <a href="/" id="anchor">Home</a>
-          <a href="/create" id="anchor">New Article</a>
+        <Link to ="/" id="anchor">Home</Link>
+          <Link to ="/create" id="anchor">New Article</Link>
         </div>  
         </nav>
      );

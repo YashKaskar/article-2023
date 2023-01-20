f3ede63f2844f3956b3189ea0422cba36cab8dd2
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-20 14:42:37 +0530

    controlled inputs

--- src/components/create.js
+++ src/components/create.js
@@ -1,18 +1,36 @@
+import { useState } from 'react';
+
+
 const Create = () => {
+
+
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+    const [author, setAuthor] = useState();
+    
+
     return (    
         <div className="create">
             <h2>Create your articles</h2>
             
             <form>
                 <label>Article title</label>
-                <input type="text" required />
+                <input type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)} />
 
                 <label>Content for article </label>
-                <textarea required></textarea>
+                <textarea required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}>
+                </textarea>
 
                 <label>Written by the author </label>
-                <textarea required></textarea>
-
+            <input type="text" required
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)} />
+            
                 <button>Add article</button>
                 
             </form>

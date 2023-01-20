1c0e6ef392f7b976126273b2ec9fd8173dff54b0
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-20 14:08:27 +0530

    add new form

--- src/components/create.js
+++ src/components/create.js
@@ -1,7 +1,21 @@
 const Create = () => {
     return (    
         <div className="create">
-        <h2>Create your articles</h2>
+            <h2>Create your articles</h2>
+            
+            <form>
+                <label>Article title</label>
+                <input type="text" required />
+
+                <label>Content for article </label>
+                <textarea required></textarea>
+
+                <label>Written by the author </label>
+                <textarea required></textarea>
+
+                <button>Add article</button>
+                
+            </form>
         </div>
      );
 }

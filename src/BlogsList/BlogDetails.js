fd50eb7629928cea46a6932a667be3fe4084e233
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-20 13:46:29 +0530

    resusing the custom hooks and style

--- src/BlogsList/BlogDetails.js
+++ src/BlogsList/BlogDetails.js
@@ -1,15 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from './../components/useFetch';
 
 
 
 const BlogDetails = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const { data : blog, isPending, error } = useFetch('http://localhost:8080/blogs/'+ id)
    
   
 
   return (
       <div>  
-          <h2>Blog Details - { id }</h2>
+      {isPending && <div> Loading..... </div>}
+      {error && <div> {error} </div>}
+      {blog && (  
+        <article> 
+          <h2 className="title">{blog.title}</h2>
+          <p className="pauthor">Writen by the author : <span>{blog.author}</span></p>
+          <div> { blog.body }</div>
+        </article>
+      )}
      </div>
   );
 }

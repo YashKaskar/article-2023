341ca91580754d77e162570ef3b13c4969e7ffb9
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-21 12:57:57 +0530

    delete button and on Click handle

--- src/BlogsList/BlogDetails.js
+++ src/BlogsList/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from './../components/useFetch';
 
 
@@ -6,8 +6,17 @@ import useFetch from './../components/useFetch';
 const BlogDetails = () => {
   const { id } = useParams();
   const { data : blog, isPending, error } = useFetch('http://localhost:8080/blogs/'+ id)
-   
   
+  const history = useHistory()
+
+
+  const handleClick = () => {
+    fetch('http://localhost:8080/blogs/' + blog.id, {
+      method: 'Delete'
+    }).then(() => { 
+      history.push('/')
+    })
+  }
 
   return (
       <div>  
@@ -17,7 +26,8 @@ const BlogDetails = () => {
         <article> 
           <h2 className="title">{blog.title}</h2>
           <p className="pauthor">Written by the author : <span>{blog.author}</span></p>
-          <div> { blog.body }</div>
+          <div> {blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
      </div>
